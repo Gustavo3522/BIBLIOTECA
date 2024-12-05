bb19edc733a0fba45e7bc81fc623dd7cbf0fc8a3
Gustavo Macedo <gustavo3vieira@gmail.com>
2024-12-05 13:48:39 -0300

    Criando a classe (Usuario)

---
+++ src/Usuario.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario {
+    private  String name;
+    private  String id;
+    private List<Livros> livrosEmprestados;
+
+    public Usuario(String name, String id){
+        this.name = name;
+        this.id = id;
+        this.livrosEmprestados = new ArrayList<>();
+    }
+
+    public void removerLivros(Livros livros){
+        livrosEmprestados.remove(livros);
+    }
+
+    public void addLivros(Livros livros){
+        livrosEmprestados.add(livros);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getId(){
+        return id;
+    }
+}
